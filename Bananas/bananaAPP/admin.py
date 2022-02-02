e3bb588e1cbd98a22248dfab9a80fef4adf2cc93
karonion <EsyAtq2RghbWbLw>
2022-02-02 21:20:25 +0200

    Настроены отображения в админпанели. Изменены виджеты админпанели Удалена модель "Юзеры"

--- Bananas/bananaAPP/admin.py
+++ Bananas/bananaAPP/admin.py
@@ -1,23 +1,21 @@
 from django.contrib import admin
-from .models import Articles, Feedback, User
+from .models import Articles, Feedback
 from django_summernote.admin import SummernoteModelAdmin
 
 
 # Класс Summernote наследует admin.ModelAdmin. Для добавления TextArea Summernote
 class SummernoteModelForTextArea(SummernoteModelAdmin):
-    summernote_fields = ('text', 'preview')
-    list_display = ('title', 'preview', 'created_by', 'created_date')
-    fields = ('title', 'preview', 'text', 'created_by', 'created_date', 'rank', 'category')
+    summernote_fields = ('text',)
+    list_display = ('title', 'preview', 'created_date')
+    fields = ('title', 'preview', 'text', 'created_by', 'created_date', 'category')
 
 
-'''Для изменения отображения данных без класса SummerNote'''
-'''class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'preview', 'created_by', 'created_date')
-    fields = ('name', 'preview', 'text', 'created_by', 'created_date', 'rank', 'category')'''
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('contact', 'date')
+    fields = ('contact', 'text', 'date')
 
 
-admin.site.register(Feedback)
-admin.site.register(User)
+admin.site.register(Feedback, FeedbackAdmin)
 admin.site.register(Articles, SummernoteModelForTextArea)
 
 
--- Bananas/bananaAPP/models.py
+++ Bananas/bananaAPP/models.py
@@ -4,13 +4,12 @@ from django.db import models
 
 # Посты
 class Articles(models.Model):
-    title = models.TextField(max_length=100)
-    created_by = models.TextField(default='NULL')
+    title = models.CharField(max_length=100)
+    created_by = models.CharField(default='NULL', max_length=255)
     created_date = models.DateTimeField(default=django.utils.timezone.now, null=False)
-    rank = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
-    preview = models.TextField(max_length=255)
+    preview = models.CharField(max_length=255)
     text = models.TextField()
     category = models.CharField(default='NULL', max_length=255)
     image = models.ImageField(upload_to='downloads')
@@ -27,16 +26,4 @@ class Feedback(models.Model):
 
     def __str__(self):
         return self.contact
-
-
-# Юзеры
-class User(models.Model):
-    first_name = models.CharField(max_length=255, null=False)
-    last_name = models.CharField(max_length=255, null=False)
-    email = models.EmailField(max_length=255, null=False)
-    password_hash = models.CharField(max_length=255, null=False)
-    created_date = models.DateTimeField(default=django.utils.timezone.now)
-
-    def __str__(self):
-        return self.email
 
